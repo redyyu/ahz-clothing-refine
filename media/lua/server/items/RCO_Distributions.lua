5768cf47d266bde9313694487bf7b2bbfa5aee8d
redyyu <redy.ru@gmail.com>
2024-03-28 23:04:11 +0800

    xxx

---
+++ media/lua/server/items/RCO_Distributions.lua
@@ -0,0 +1,108 @@
+require 'Items/ProceduralDistributions'
+
+local function insertDistribution(list, distributionsMap)
+    for item_name, places in pairs(distributionsMap) do
+        for place_name, weight in pairs(places) do
+            local dist_to = list[place_name]
+            if dist_to then
+                if dist_to.items then
+                    dist_to = dist_to.items
+                end
+                table.insert(dist_to, item_name)
+                table.insert(dist_to, weight)
+            end
+        end
+    end
+end
+
+
+local skirtsDistributionsMap = {
+    ["Stockings_Fitness"] = {
+        ["ClothingStoresSocks"] = 10,
+        ["ClothingStoresSport"] = 10,
+        ["CrateClothesRandom"] = 1,
+        ["GymLockers"] = 10,
+        ["GymLaundry"] = 10,
+        ["LaundryLoad6"] = 5,
+    },
+    ["Sweater_FitnessArms"] = {
+        ["ClothingStoresSport"] = 10,
+        ["CrateClothesRandom"] = 1,
+        ["GymLockers"] = 10,
+        ["GymLaundry"] = 10,
+        ["LaundryLoad6"] = 5,
+    },
+    ["Bar_Fitness"] = {
+        ["ClothingStoresUnderwearWoman"] = 10,
+        ["ClothingStoresSport"] = 10,
+        ["CrateClothesRandom"] = 1,
+        ["GymLockers"] = 10,
+        ["GymLaundry"] = 10,
+        ["LaundryLoad6"] = 5,
+    },
+    ["Underpants_Fitness"] = {
+        ["ClothingStoresUnderwearWoman"] = 10,
+        ["ClothingStoresSport"] = 10,
+        ["CrateClothesRandom"] = 1,
+        ["GymLockers"] = 10,
+        ["GymLaundry"] = 10,
+        ["LaundryLoad6"] = 5,
+    },
+    ["Underpants_HighWaistT"] = {
+        ["LingerieStoreOutfits"] = 3,
+        ["ClothingStoresUnderwearWoman"] = 3,
+        ["CrateClothesRandom"] = 1,
+        ["LaundryLoad7"] = 3, 
+        ["StripClubDressers"] = 15,
+    },
+    ["Underpants_TightShorts"] = {
+        ["LingerieStoreOutfits"] = 3,
+        ["ClothingStoresUnderwearWoman"] = 3,
+        ["CrateClothesRandom"] = 1,
+        ["LaundryLoad7"] = 3, 
+        ["StripClubDressers"] = 15,
+    },
+    ["Tshirt_LongSleeveTop"] = {
+        ["ClothingStoresWoman"] = 4,
+        ["BedroomDresser"] = 1,
+        ["DresserGeneric"] = 2,
+        ["WardrobeWoman"] = 4, 
+        ["CrateClothesRandom"] = 4,
+    },
+    ["Tshirt_LongSleeveTopCut"] = {
+        ["ClothingStoresWoman"] = 2,
+        ["BedroomDresser"] = 0.5,
+        ["DresserGeneric"] = 1,
+        ["WardrobeWoman"] = 2, 
+        ["CrateClothesRandom"] = 2,
+    },
+    ["Skirt_ShortPleated"] = {
+        ["ClothingStoresSummer"] = 4,
+        ["LingerieStoreOutfits"] = 1,
+        ["ClothingStorageLegwear"] = 0.5,
+        ["ClothingStoresDress"] = 2
+        ["ClothingStoresWoman"] = 2
+        ["SchoolLockers"] = 0.2,
+        ["BedroomDresser"] = 0.5,
+        ["DresserGeneric"] = 0.5,
+        ["ClosetShelfGeneric"] = 0.2,
+        ["GymLockers"] = 0.5,
+        ["GymLaundry"] = 2,
+        ["LaundryLoad1"] = 20,
+        ["LaundryLoad2"] = 20,
+        ["LaundryLoad3"] = 20,
+        ["LaundryLoad4"] = 20,
+        ["LaundryLoad5"] = 20,
+        ["LaundryLoad6"] = 20,
+        ["LaundryLoad7"] = 20,
+        ["LaundryLoad8"] = 20,
+        ["Locker"] = 0.1,
+        ["StripClubDressers"] = 15,
+        ["WardrobeWoman"] = 6, 
+        ["CrateClothesRandom"] = 2,
+    },
+    
+}
+
+insertDistribution(ProceduralDistributions.list, skirtsDistributionsMap)
+
